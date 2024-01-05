07730f6a88b8860295e8d918686810122f62f530
Sama Ashraf <60572513+Sama147@users.noreply.github.com>
2024-01-05 05:00:31 +0200

    Update Assignment1_ code.java

--- Assignment1_ code.java
+++ Assignment1_ code.java
@@ -1,25 +1,66 @@
+import java.util.Scanner;
+	import java.text.NumberFormat;
 
-import java.util.Scanner;   	 //Calling for scanner from Java library to read user input
+	public class Assignment2 {
+	    public static void main(String[] args) {
+	        final double HAMBURGER_PRICE = 2.75;
+	        final double CHEESEBURGER_PRICE = 3.25;
+	        final double FRIES_PRICE = 2.50;
+	        final double DRINK_PRICE = 1.50;
 
-public class Assignment1{
-	
+	        Scanner input = new Scanner(System.in);
+	        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
 
-	public static void main(String[] args) {
-		Scanner scanner= new Scanner (System.in);               //Utilizing scanner to save input entered by the user
-		System.out.print("Enter the bill amount: $");           //User enters bill amount
-		double bill= scanner.nextDouble();                      //assigning entered bill in variable bill
-		
-		System.out.print("What percent would you like to tip: ");//User enters percent of the tip
-		double tip_percent=scanner.nextDouble();                 //Assigning entered value in variable tip
-		
-		double tip=(bill*tip_percent)/100;                       //variable calculating tip percent
-		System.out.println("Tip Amount: $" + tip ); //printing out the tip amount 
-		double total= tip+ bill;                         //variable with calculated total amount of bill
-		System.out.println("Total amount with tip: $"+ total);   //printing out total bill 
-		
-    }
-}
+	        System.out.println("Welcome to the In-N-Out Burger menu:");
+	        System.out.println("------------------------------------------------------");
+	        System.out.println("Hamburger: " + currencyFormat.format(HAMBURGER_PRICE));
+	        System.out.println("Cheeseburger: " + currencyFormat.format(CHEESEBURGER_PRICE));
+	        System.out.println("French Fries: " + currencyFormat.format(FRIES_PRICE));
+	        System.out.println("Shake & Beverage: " + currencyFormat.format(DRINK_PRICE));
 
+	        System.out.print("How many hamburger(s) would you like to buy? ");
+	        int numHamburgers = input.nextInt();
 
+	        System.out.print("How many cheeseburger(s) would you like to buy? ");
+	        int numCheeseburgers = input.nextInt();
 
- 
+	        System.out.print("How many French fries would you like to buy? ");
+	        int numFries = input.nextInt();
+
+	        System.out.print("How many drinks would you like to buy? ");
+	        int numDrinks = input.nextInt();
+
+	        double totalHamburgerCost = numHamburgers * HAMBURGER_PRICE;
+	        double totalCheeseburgerCost = numCheeseburgers * CHEESEBURGER_PRICE;
+	        double totalFriesCost = numFries * FRIES_PRICE;
+	        double totalDrinkCost = numDrinks * DRINK_PRICE;
+
+	        String itemWithHighestTotalCost = "hamburgers";
+	        double highestTotalCost = totalHamburgerCost;
+
+	        if (totalCheeseburgerCost > highestTotalCost) {
+	            itemWithHighestTotalCost = "cheeseburgers";
+	            highestTotalCost = totalCheeseburgerCost;
+	        }
+	        if (totalFriesCost > highestTotalCost) {
+	            itemWithHighestTotalCost = "fries";
+	            highestTotalCost = totalFriesCost;
+	        }
+	        if (totalDrinkCost > highestTotalCost) {
+	            itemWithHighestTotalCost = "drinks";
+	            highestTotalCost = totalDrinkCost;
+	        }
+
+	        double totalOrderCost = totalHamburgerCost + totalCheeseburgerCost + totalFriesCost + totalDrinkCost;
+	        int totalItemsOrdered = numHamburgers + numCheeseburgers + numFries + numDrinks;
+
+	        System.out.println("Total cost for the hamburger(s): " + currencyFormat.format(totalHamburgerCost));
+	        System.out.println("Total cost for the cheeseburger(s): " + currencyFormat.format(totalCheeseburgerCost));
+	        System.out.println("Total cost for the fries: " + currencyFormat.format(totalFriesCost));
+	        System.out.println("Total cost for the drink(s): " + currencyFormat.format(totalDrinkCost));
+	        System.out.println("The " + itemWithHighestTotalCost + " had the highest total cost.");
+	        System.out.println("Total cost for your order: " + currencyFormat.format(totalOrderCost));
+	        System.out.println("Total number of item(s) ordered: " + totalItemsOrdered);
+	    }
+	}
+
